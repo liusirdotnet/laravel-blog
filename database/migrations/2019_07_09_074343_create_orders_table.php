a03ab0bf7181c251565029b59f54592bd05bfb4d
liusirdotnet <17611592337@163.com>
2019-07-09 15:55:07 +0800

    init orders migrations

---
+++ database/migrations/2019_07_09_074343_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->decimal('amount', 10, 2);
+            $table->enum('status', ['待支付', '已支付', '已退款']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
